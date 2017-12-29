665cf6fcef0712a0de005700b26d9121ee2ead65
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2017-12-29 14:57:16 -0300

    Set test script

--- truffle.js
+++ truffle.js
@@ -1,9 +1,5 @@
 module.exports = {
   networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-    }
+    // add network configurations here
   }
 }
