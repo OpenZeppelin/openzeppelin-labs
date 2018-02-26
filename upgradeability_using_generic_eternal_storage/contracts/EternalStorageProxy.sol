6b05a83aee3eb7ac2c1f235e14f4b07c08afeb1f
Roman Storm <rstormsf@gmail.com>
2018-02-25 21:39:20 -0800

    Change Comment in contracts/EternalStorageProxy.sol

--- upgradeability_using_generic_eternal_storage/contracts/EternalStorageProxy.sol
+++ upgradeability_using_generic_eternal_storage/contracts/EternalStorageProxy.sol
@@ -5,7 +5,7 @@ import "./OwnedUpgradeabilityProxy.sol";
 
 
 /**
- * @title TokenProxy
+ * @title EternalStorageProxy
  * @dev This proxy holds the storage of the token contract and delegates every call to the current implementation set.
  * Besides, it allows to upgrade the token's behaviour towards further implementations, and provides basic
  * authorization control functionalities
