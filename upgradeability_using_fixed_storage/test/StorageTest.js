b622dc8b46e1ea97c97f38dc352ef5d0ae804fee
Alejo Salles <alejosalles@gmail.com>
2018-04-03 21:49:38 -0300

    added test to demonstrate storage location

---
+++ upgradeability_using_fixed_storage/test/StorageTest.js
@@ -0,0 +1,28 @@
+
+const encodeCall = require('./helpers/encodeCall')
+const OwnedUpgradeabilityProxy = artifacts.require('OwnedUpgradeabilityProxy')
+const Token_V0 = artifacts.require('Token_V0')
+
+contract('OwnedUpgradeabilityProxy', ([_, proxyOwner, tokenOwner, anotherAccount]) => {
+  let proxy
+  let impl_v0
+  let impl_v1
+  let token_v0
+  let token_v1
+  const initializeData = encodeCall('initialize', ['address'], [tokenOwner]);
+
+  beforeEach(async function () {
+    proxy = await OwnedUpgradeabilityProxy.new({ from: proxyOwner })
+    impl_v0 = await Token_V0.new()
+    token_v0 = Token_V0.at(proxy.address)
+    const initializeData = encodeCall('initialize', ['address'], [tokenOwner])
+    await proxy.upgradeToAndCall(impl_v0.address, initializeData, { from: proxyOwner })
+  });
+
+  it('should store implementation in specified location', async function () {
+    var position = web3.sha3("org.zeppelinos.proxy.implementation");
+    var storage = await web3.eth.getStorageAt(proxy.address, position);
+    assert.equal(storage, impl_v0.address);
+  });
+  
+});
