3f8a0ec97c377c026d1024832fa0d3f382faf0d3
Santiago Palladino <spalladino@gmail.com>
2018-04-04 16:21:48 -0300

    Split staking basic logic to a KernelStakes separate contract

---
+++ kernel/contracts/KernelStakes.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.4.18;
+
+import "./ZepToken.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract KernelStakes is  Ownable {
+  using SafeMath for uint256;
+
+  event Staked(address indexed user, address instance, uint256 amount, uint256 total, bytes data);
+  event Unstaked(address indexed user, address instance, uint256 amount, uint256 total, bytes data);
+
+  uint256 private _totalStaked;
+
+  mapping(address => uint256) private _instanceVouches;
+  mapping(address => mapping (address => uint256)) private _userVouches;
+
+  function KernelStakes() { }
+
+  function stake(address staker, address instance, uint256 amount, bytes data) public onlyOwner {
+    _totalStaked = _totalStaked.add(amount);
+    _instanceVouches[instance] = _instanceVouches[instance].add(amount);
+    _userVouches[staker][instance] = _userVouches[staker][instance].add(amount);
+
+    Staked(staker, instance, amount, _instanceVouches[instance], data);
+  }
+
+  function unstake(address staker, address instance, uint256 amount, bytes data) public onlyOwner {
+    uint256 currentStake = _userVouches[staker][instance];
+    require(currentStake >= amount);
+
+    _totalStaked = totalStaked().sub(amount);
+    _instanceVouches[instance] = _instanceVouches[instance].sub(amount);
+    _userVouches[staker][instance] = currentStake.sub(amount);
+    
+    Unstaked(staker, instance, amount, _instanceVouches[instance], data);
+  }
+
+  function transferStake(address staker, address from, address to, uint256 amount, bytes data) public onlyOwner {
+    unstake(staker, from, amount, data);
+    stake(staker, to, amount, data);
+  }
+
+  function stakedFor(address staker, address instance) public view returns (uint256) {
+    return _userVouches[staker][instance];
+  }
+
+  function totalStakedFor(address instance) public view returns (uint256) {
+    return _instanceVouches[instance];
+  }
+
+  function totalStaked() public view returns (uint256) {
+    return _totalStaked;
+  }
+}
--- kernel/contracts/ZepCore.sol
+++ kernel/contracts/ZepCore.sol
@@ -3,77 +3,60 @@ pragma solidity ^0.4.18;
 import "./ZepToken.sol";
 import "./KernelInstance.sol";
 import "./KernelRegistry.sol";
+import "./KernelStakes.sol";
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract ZepCore {
   using SafeMath for uint256;
 
-  event Staked(address indexed user, KernelInstance instance, uint256 amount, uint256 total, bytes data);
-  event Unstaked(address indexed user, KernelInstance instance, uint256 amount, uint256 total, bytes data);
+  ZepToken private _token;
+  KernelRegistry private _registry;
+  KernelStakes private _stakes;
 
   uint256 public newVersionCost;
   uint256 public developerFraction;
 
-  ZepToken private _token;
-  KernelRegistry private _registry;
-  uint256 private _totalStaked;
-  mapping(address => uint256) private _instanceVouches;
-  mapping(address => mapping (address => uint256)) private _userVouches;
-
   function ZepCore(uint256 _newVersionCost, uint256 _developerFraction) public {
-    _token = new ZepToken();
     _registry = new KernelRegistry();
+    _stakes = new KernelStakes();
+    _token = new ZepToken();
     _token.transferOwnership(msg.sender);
+    
     developerFraction = _developerFraction;
     newVersionCost = _newVersionCost;
     // TODO: we need to think how we are going to manage variable costs to propose new versions
   }
 
   function register(KernelInstance instance) public {
-    registry().addInstance(instance);
-    token().transferFrom(msg.sender, this, newVersionCost);
-    token().burn(newVersionCost);
-    // TODO: should we build a burnFrom or sth like that?
+    _registry.addInstance(instance);
+    
+    // TODO: Update to burnFrom once https://github.com/OpenZeppelin/zeppelin-solidity/pull/870 is merged
+    _token.transferFrom(msg.sender, this, newVersionCost);
+    _token.burn(newVersionCost);
   }
 
-  function transferStake(KernelInstance from, KernelInstance to, uint256 amount, bytes data) public {
-    // TODO: an unstake followed by a stake call won't work
-    require(false);
+  function getInstance(string name, string version) public view returns(KernelInstance) {
+    return _registry.getInstance(name, version);
   }
 
   function stake(KernelInstance instance, uint256 amount, bytes data) public {
-    token().transferFrom(msg.sender, this, amount);
+    _token.transferFrom(msg.sender, this, amount);
     uint256 developerPayout = amount.div(developerFraction);
     require(developerPayout > 0);
-    // TODO: check how we can manage remainders in a better way
+    // TODO: Think how we can manage remainders in a better way
 
     uint256 stakedAmount = amount.sub(developerPayout);
-    _totalStaked = totalStaked().add(stakedAmount);
-    _instanceVouches[instance] = totalStakedFor(instance).add(stakedAmount);
-    _userVouches[msg.sender][instance] = _userVouches[msg.sender][instance].add(stakedAmount);
-    token().transfer(instance.developer(), developerPayout);
-
-    Staked(msg.sender, instance, stakedAmount, totalStakedFor(instance), data);
+    _stakes.stake(msg.sender, instance, stakedAmount, data);
+    _token.transfer(instance.developer(), developerPayout);
   }
 
   function unstake(KernelInstance instance, uint256 amount, bytes data) public {
-    uint256 currentStake = _userVouches[msg.sender][instance];
-    require(currentStake >= amount);
-
-    _totalStaked = totalStaked().sub(amount);
-    _instanceVouches[instance] = totalStakedFor(instance).sub(amount);
-    _userVouches[msg.sender][instance] = currentStake.sub(amount);
-    token().transfer(msg.sender, amount);
-    
-    Unstaked(msg.sender, instance, amount, totalStakedFor(instance), data);
+    _stakes.unstake(msg.sender, instance, amount, data);
+    _token.transfer(msg.sender, amount);    
   }
 
-  function totalStakedFor(KernelInstance instance) public view returns (uint256) {
-    return _instanceVouches[instance];
-  }
-
-  function totalStaked() public view returns (uint256) {
-    return _totalStaked;
+  function transferStake(KernelInstance from, KernelInstance to, uint256 amount, bytes data) public {
+    _stakes.transferStake(msg.sender, from, to, amount, data);
   }
 
   function token() public view returns (ZepToken) {
@@ -82,5 +65,9 @@ contract ZepCore {
 
   function registry() public view returns (KernelRegistry) {
     return _registry;
+  }
+
+  function stakes() public view returns (KernelStakes) {
+    return _stakes;
   }
 }
