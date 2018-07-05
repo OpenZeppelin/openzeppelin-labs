513c8741932da058c665625bd7e45fed6829245e
Francisco Giordano <frangio.1@gmail.com>
2018-07-05 19:52:19 -0300

    add explanation of bytecode

--- initializer_contracts/scripts/poc-1.js
+++ initializer_contracts/scripts/poc-1.js
@@ -28,6 +28,23 @@ async function deploy(bytecode) {
   return receipt.contractAddress;
 }
 
+// This is EVM assembly that, when used at the beginning of a contract, will
+// return all of the code that follows it.
+// 
+// operation | bytecode   | stack representation
+// =================================================
+// push1 0C  | 0x60 0x0D  | 0x0C
+// dup1      | 0x80       | 0x0C 0x0C
+// codesize  | 0x38       | 0x0C 0x0C 0xCS
+// sub       | 0x03       | 0x0C 0xIS
+// dup1      | 0x80       | 0x0C 0xIS 0xIS
+// dup3      | 0x82       | 0x0C 0xIS 0xIS 0x0C
+// push1 00  | 0x60 0x00  | 0x0C 0xIS 0xIS 0x0C 0x00
+// codecopy  | 0x39       | 0x0C 0xIS
+// push1 00  | 0x60 0x00  | 0x0C 0xIS 0x00
+// return    | 0xf3
+const ASM_RETURN_REST = '0x600d80380380826000396000f3';
+
 async function main() {
   await run('compile');
 
@@ -35,7 +52,7 @@ async function main() {
   const AdminUpgradeabilityProxy = artifacts.require('AdminUpgradeabilityProxy');
 
   const implementation = await deploy(Greeter.bytecode);
-  const constructor = await deploy(Greeter.bytecode.replace('0x', '0x600d80380380826000396000f3'));
+  const constructor = await deploy(Greeter.bytecode.replace('0x', ASM_RETURN_REST));
 
   const admin = (await pweb3.eth.getAccounts())[1];
   const proxy = await AdminUpgradeabilityProxy.new(constructor, implementation, { from: admin });
