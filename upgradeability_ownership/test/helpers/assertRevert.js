4f14d95334daa7ee884511eca5c0ad6861a51f38
YJ <yjkimjunior@gmail.com>
2018-07-08 14:38:54 -0700

    WIP: modified assertRevert helper

--- upgradeability_ownership/test/helpers/assertRevert.js
+++ upgradeability_ownership/test/helpers/assertRevert.js
@@ -1,5 +1,10 @@
-async function assertRevert(promise) {
+async function assertRevert(promise, errMsg) {
   try {
+    if (typeof errMsg !== "undefined") {
+      console.log(errMsg);
+      const errMsgFound = errMsg.search('from the contract') >= 0;
+      assert(errMsgFound, `Expected "reason provided from the contract", got ${errMsg} instead`);
+    }
     await promise;
     assert.fail('Expected revert not received');
   } catch (error) {
