2c956bfedaa03eaecd631659eedd78a14c8db6f9
Francisco Giordano <frangio.1@gmail.com>
2022-06-07 10:41:29 -0300

    cleanup

--- crosschain-contracts/contracts/ICrossChainEmitter.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface ICrossChainEmitter {
-    event CrossChainTxSent(address indexed target, uint256 value, bytes data);
-
-    function pair() external view returns (address);
-    function sendCrossChainTx(address target, uint256 value, bytes calldata data, uint32 gasLimit) external;
-}
--- crosschain-contracts/contracts/ICrossChainReceiver.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface ICrossChainReceiver {
-    event CrossChainTxReceived(address indexed target, uint256 value, bytes data);
-
-    function pair() external view returns (address);
-    function receiveCrossChainTx(address target, uint256 value, bytes calldata data) external;
-}
